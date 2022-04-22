327fc85da703c4075470d8948cb43fbaa2f2ab3b
Artem Gordienko <66356412+MrRedHand@users.noreply.github.com>
2022-04-22 14:31:51 +0300

    asd

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,16 +3,21 @@ import Sidebar from '../sidebar/sidebar';
 import SeminarsList from '../seminars-list/seminars-list';
 import TrajectoryBanner from "../trajectory-banner/trajectory-banner";
 
-const jsonData = require('../../data/seminars.json')
-
-console.log(jsonData)
-
 export default function App() {
+
+  const jsonData  = require('../../data/seminars.json')
+
+  console.log(jsonData)
+
+  const tester = (name) => {
+    console.log('Вывзвано', name)
+  }
+
   return (
     <div className="container seminars-list-page">
         <div className='row'>
           <div className='col-lg-3'>
-              <Sidebar jsonData={jsonData}/>
+              <Sidebar jsonData={jsonData} testFunc={tester}/>
           </div>
           <div className='col-lg-9'>
               <TrajectoryBanner />
--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,35 +1,15 @@
 import React, { useEffect } from 'react'
-import PropTypes from 'prop-types';
 import st from './input.module.css'
 
 const Input = (props) => {
 
 
     return (
-        <label className={`${props.labelClass}`} onClick={() => console.log('asdasd')}>
-            <span 
-                className={`${props.spanClass}`}>
-                    {props.spanDescription}
-            </span>
-            <input 
-                className={`${props.inputClass}`} 
-                placeholder={`${props.placeholder}`} 
-                type={`${props.type}`} 
-                name={`${props.name}`} 
-                />
+        <label className="filter__label" >
+            <input type="checkbox"/><span className="span-value">{props.value}</span>
         </label>
     )
 }
 
-Input.propTypes = {
-    labelClass: PropTypes.string,
-    spanClass: PropTypes.string,
-    inputClass: PropTypes.string,
-    spanDescription: PropTypes.string,
-    placeholder: PropTypes.string,
-    type: PropTypes.string,
-    name: PropTypes.string,
-    value: PropTypes.string,
-}
 
 export default Input
